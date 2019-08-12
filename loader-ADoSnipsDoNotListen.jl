615bfb259c7e5d9f9907ae14dacf1e24736f6965
andreasdominik <andreas.dominik@mni.thm.de>
2019-08-12 17:00:24 +0200

    Inital commit for new Skill ADoSnipsDoNotListen

--- loader-ADoSnipsDoNotListen.jl
+++ loader-ADoSnipsDoNotListen.jl
@@ -8,8 +8,8 @@
 #
 
 APP_DIR = @__DIR__
-include("$APP_DIR/Skill/ADoSnipsTemplate.jl")
-import Main.ADoSnipsTemplate
+include("$APP_DIR/Skill/ADoSnipsDoNotListen.jl")
+import Main.ADoSnipsDoNotListen
 
 global INTENT_ACTIONS
-append!(INTENT_ACTIONS, ADoSnipsTemplate.getIntentActions())
+append!(INTENT_ACTIONS, ADoSnipsDoNotListen.getIntentActions())
